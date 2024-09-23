52d43a3cd6ab345f71d58d0013b257da5583f626
HeatherKates <hkates@ufl.edu>
2024-09-23 00:35:43 -0400

    update all scripts to array scripts

--- scripts/1_copy_rawreads.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ../
-cp /orange/zhangw/NS3842/NS-3842-WZhang-15B-223353LT1-Lane1-2/NS-3842_7_231-MSH2KO-1_N702_N503_L001/* 1_raw_reads/
---
+++ scripts/2_multiQC.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run multiqc to summarize the fastqc results
+multiqc 2b_trimgalore_fastqc/ -o 2b_trimgalore_fastqc/
