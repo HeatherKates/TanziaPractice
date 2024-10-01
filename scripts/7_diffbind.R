313839e0732f0dcbb4ec1f6387cc11e4263d4c1c
HeatherKates <hkates@ufl.edu>
2024-10-01 10:32:36 -0400

    add README for diffbind results

--- scripts/7_diffbind.R
+++ scripts/7_diffbind.R
@@ -7,7 +7,7 @@ bp_param <- MulticoreParam(workers = 6)
 
 samples <- data.frame(
   SampleID = c("MSH2KO-1", "MSH2KO-2", "MSH2KO-3", "MSH2R4-1", "MSH2R4-2", "MSH2R4-3"),
-  Tissue = c(rep("KO", 3), rep("MSH2", 3)),  # You can change 'Tissue' to represent conditions or leave this column out.
+  Tissue = c(rep("KO", 3), rep("MSH2", 3)),  #  'Tissue' can represent conditions or leave this column out.
   Factor = c(rep("MSH2KO", 3), rep("MSH2R4", 3)),
   Condition = c(rep("MSH2KO", 3), rep("MSH2", 3)),  # The experimental conditions
   Treatment = rep("None", 6),  # Since there's no treatment in this setup
@@ -16,7 +16,7 @@ samples <- data.frame(
                "../6_bigwig/MSH2R4-1_sort_n.bam", "../6_bigwig/MSH2R4-2_sort_n.bam", "../6_bigwig/MSH2R4-3_sort_n.bam"),
   Peaks = c("../5_genrich/MSH2KO-1.narrowPeak", "../5_genrich/MSH2KO-2.narrowPeak", "../5_genrich/MSH2KO-3.narrowPeak",
             "../5_genrich/MSH2R4-1.narrowPeak", "../5_genrich/MSH2R4-2.narrowPeak", "../5_genrich/MSH2R4-3.narrowPeak"),
-  PeakCaller = rep("narrow", 6),  # Since you're using Genrich, 'narrow' for narrowPeak format
+  PeakCaller = rep("narrow", 6),  # For Genrich, 'narrow' for narrowPeak format
   stringsAsFactors = FALSE
 )
 
@@ -53,3 +53,28 @@ bed <- data.frame(
 
 # Write the BED file
 write.table(bed, file = "../7_diffbind/differential_binding_results.bed", quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+
+#Make a README
+# Define column names and their descriptions
+column_descriptions <- data.frame(
+  Heading = c("X", "seqnames", "start", "end", "width", "strand", "Conc", 
+              "Conc_MSH2", "Conc_KO", "Fold", "p.value", "FDR"),
+  Description = c(
+    "Row index or identifier (not relevant for analysis)",
+    "Chromosome or contig name where the peak is located",
+    "Start position of the peak on the chromosome",
+    "End position of the peak on the chromosome",
+    "Width of the peak (end - start)",
+    "Strand information (+/-) where the peak is located, if applicable",
+    "Average read concentration (normalized counts) across all samples",
+    "Average read concentration in MSH2 samples (wild-type or tagged)",
+    "Average read concentration in KO (knockout) samples",
+    "Log2 fold change of read concentration between KO and MSH2 samples",
+    "Raw p-value for differential binding between KO and MSH2 samples",
+    "False discovery rate (adjusted p-value) for differential binding"
+  ),
+  stringsAsFactors = FALSE
+)
+
+# View the data frame
+print(column_descriptions)
